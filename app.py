ec861c48531ca354706a52a197e1bff3bc7a5275
yash2189 <yashajgaonkar24@gmail.com>
2020-06-11 19:26:54 +0530

    :rocket: Updated Code

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ import datetime as dt
 
 app = Flask(__name__)
 
+#Your API Key
+API_KEY = '5f1348e8f6aa675ecc09c5bafd5cf2d3'
 
-
-@app.route('/')
+@app.route('/',methods = ["POST","GET"])
 def search_city():
-    API_KEY = '5f1348e8f6aa675ecc09c5bafd5cf2d3'
-    city = 'Seoul'
-    units = 'Metric'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}&units={units}'
-    response = requests.get(url).json()
+    if request.method == "POST":
+        city = request.form.get("city")
+        if city == "":
+            return render_template("error.html")
+        units = 'Metric'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}&units={units}'
+        response = requests.get(url).json()
 
-    weather = {
-        'city': city,
-        'temperature': response["main"]["temp"],
-        'description':response["weather"][0]["description"],
-        'icon':response["weather"][0]["icon"],
-        'humidity':response["main"]["humidity"],
-    }
-
-
-    return render_template("weather_new.html",weather=weather)
+        
+        new_city = city 
+        temperature = response["main"]["temp"]
+        description = response["weather"][0]["description"]
+        icon = response["weather"][0]["icon"]
+        humidity = response["main"]["humidity"]
+        
+        return render_template("weather_new.html",city = new_city,temperature = temperature,description = description,icon = icon,humidity=humidity)
+    return render_template("weather_new.html")
 if __name__ == '__main__':
     app.debug = True
     app.run()
